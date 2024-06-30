07f017a13f8085a7d5310f57cc97ab840ad16238
sarahvar <sarahvarin95@gmail.com>
2024-06-30 21:55:43 +0200

    Modal_respect_jquery

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -4,9 +4,10 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) {
     return null;
   }
+
   return (
     <div id="modal" className="modal">
-      <div className="modal-overlay"></div>
+      <div className="modal-overlay" onClick={onClose}></div>
       <div className="modal-container">
         <div className="modal-header">
           <button onClick={onClose} className="close-button">
@@ -25,4 +26,5 @@ const Modal = ({ isOpen, onClose, children }) => {
 };
 
 export default Modal;
+
 
