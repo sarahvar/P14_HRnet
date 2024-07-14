6a3e4fd4d890d3cd8e362df4368b0978629949ab
sarahvar <sarahvarin95@gmail.com>
2024-07-14 19:54:04 +0200

    Employees

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import './App.css';
 
 
 const Home = lazy(() => import("../src/Pages/Home/Home"));
-
+const Employees = lazy(() => import("../src/Pages/Employees/Employees"));
 const App = () => (
   <>
    <Suspense fallback={<div>Loading...</div>}>
     <Header />
     <Routes>
-      <Route index element={<Home />} />
+      <Route path ="/" element={<Home />} />
+      <Route path="/employees" element={<Employees />} />
     </Routes>
     </Suspense>
   </>
---
+++ src/Pages/Employees/Employees.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+import { Link } from "react-router-dom";
+import MyTable from "../../components/Table/MyTable";
+import labels from "../../../data/labels";
+import "./Employees.css";
+
+export default function Employees() {
+  const employees = JSON.parse(localStorage.getItem("employees"));
+
+  return (
+    <div className="employees">
+      <div className="table">
+        <MyTable labels={labels} data={employees} />
+      </div>
+
+      <div className="employees-link">
+        <Link to="/"> &#x21A9; Home</Link>
+      </div>
+    </div>
+  );
+}
