b63c17635ab31ef4f49c4bcf14c9bb8813bc90ab
sarahvar <sarahvarin95@gmail.com>
2024-07-15 00:10:14 +0200

    delete

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    APP_ENV: process.env.VITE_VERCEL_ENV,
-  },
-})
+});
