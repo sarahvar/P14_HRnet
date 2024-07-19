ea8d7c5f11edbfc00aaa479836bf05c517baee80
sarahvar <sarahvarin95@gmail.com>
2024-07-19 22:40:17 +0200

    persist

--- src/Redux/Store/store.jsx
+++ src/Redux/Store/store.jsx
@@ -1,10 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import employeeSlice from "../Slice/employeeSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
+// Configuration de la persistance
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+  blacklist: ['loading', 'error'],
+};
+
+// Appliquez la configuration de persistance à employeeSlice
+const persistedEmployeeReducer = persistReducer(persistConfig, employeeSlice);
+
+// Configurez le store
 const store = configureStore({
   reducer: {
-    employee: employeeSlice,
+    employee: persistedEmployeeReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
 });
 
+// Créez une fonction pour arrêter la persistance après un délai spécifié
+const stopPersistAfterDelay = (delay) => {
+  setTimeout(() => {
+    persistStore(store).purge(); // Arrêtez la persistance des données
+  }, delay);
+};
+
+// Appelez la fonction pour arrêter la persistance après 24 heures en millisecondes
+stopPersistAfterDelay(86400000);
+
 export default store;
+export const persistor = persistStore(store);
