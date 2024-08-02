2ce641daf3755f635d94f00ecb630a6d24615270
sarahvar <sarahvarin95@gmail.com>
2024-08-02 09:24:00 +0200

    Page-Error-css

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Error.css";
 
 export default function NotFound() {
+  useEffect(() => {
+    document.body.classList.add("error-page-body");
+    return () => {
+      document.body.classList.remove("error-page-body");
+    };
+  }, []);
+
   return (
-    <div className="employees">
+    <div className="error-page">
       <div className="error">
         <div className="error-link">
           <Link to="/"> &#x21A9; Home &#x1F3E1; &#x1F3E1;</Link>
         </div>
-        <h1 className="text-8xl font-extrabold tracking-tight lg:text-9xl">
-                404
-            </h1>
-        <p>Error, this page doesn't exist!</p>
+        <h1 className="error-code">404</h1>
+        <p className="error-message">Error, this page doesn't exist!</p>
         <div className="error-link">
           <Link to="/employees">
             &#x1F4C8; &#x1F4C8; Current Employees &#x21AA;
