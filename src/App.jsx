e1d3984319e1b4633565994f538103b8a5482b2e
sarahvar <sarahvarin95@gmail.com>
2024-08-02 09:13:32 +0200

    Page-Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import './App.css';
-import Header from "./Components/Header/Header"; 
+import Header from "./Components/Header/Header";
 
 const Home = lazy(() => import("../src/Pages/Home/Home"));
 const Employees = lazy(() => import("../src/Pages/Employees/Employees"));
 const Error = lazy(() => import("../src/Pages/Error/Error"));
 
+const MainLayout = ({ children }) => {
+  const location = useLocation();
+  const isErrorPage = location.pathname === '/error';
+
+  return (
+    <>
+      {!isErrorPage && <Header />}
+      {children}
+    </>
+  );
+};
+
 const App = () => (
-  <>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Header /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/employees" element={<Employees />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Suspense>
-  </>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+      <Route path="/employees" element={<MainLayout><Employees /></MainLayout>} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </Suspense>
 );
 
 export default App;
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Error.css";
 
-export default function EmployeesBis() {
+export default function NotFound() {
   return (
     <div className="employees">
       <div className="error">
         <div className="error-link">
           <Link to="/"> &#x21A9; Home &#x1F3E1; &#x1F3E1;</Link>
         </div>
+        <h1 className="text-8xl font-extrabold tracking-tight lg:text-9xl">
+                404
+            </h1>
         <p>Error, this page doesn't exist!</p>
         <div className="error-link">
           <Link to="/employees">
